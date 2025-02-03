268faf3d5cb277ea9ae2f6ec8d937c7162ad9763
Akak <robin.white280@gmail.com>
2025-02-03 01:21:20 +0100

    add word debug info component
    update notes

---
+++ src/components/debug/WordInfo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Words } from "../../model/model";
+
+export const WordInfo: React.FC<{word: Words}> = (props) => {
+    return (
+        <>
+            <Text>Id: {props.word.id}</Text>
+            <Text>Seen: {props.word.seen}</Text>
+            <Text>Correct: {props.word.correct}</Text>
+            <Text>Ending: -{props.word.ending}</Text>
+            <Text>Freq: {props.word.frequency}</Text>
+            <Text>Last: {props.word.lastSeen}</Text>
+            <Text>Gender: {props.word.gender}</Text>
+        </>
+    );
+}
