c0f8f2023dadf93a1aafde3301597beb22ab68bd
Akak <robin.white280@gmail.com>
2025-06-16 16:42:19 +0200

    use KeyboardAvoidingView in feedback screen

--- src/app/(tabs)/settings/feedback.tsx
+++ src/app/(tabs)/settings/feedback.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput, ScrollView, StyleSheet } from 'react-native';
+import { View, TextInput, ScrollView, StyleSheet, KeyboardAvoidingView, NativeSyntheticEvent, TextInputTextInputEventData, TextInputChangeEventData } from 'react-native';
 
 import { Setting } from '../../../components/Setting';
 import { useTheme } from '../../../colors';
@@ -19,7 +19,7 @@ const style = StyleSheet.create({
 export default function Feedback() {
     const theme = useTheme();
     const [validated, setValidated] = React.useState<boolean>();
-    const [name, setName] = React.useState<string|undefined>();
+    const [name, setName] = React.useState<string | undefined>();
     const [email, setEmail] = React.useState<string | undefined>();
     const [description, setDescription] = React.useState<string | undefined>();
 
@@ -33,42 +33,48 @@ export default function Feedback() {
         return colors.green.background;
     }
 
+    function setValue(setter: React.Dispatch<React.SetStateAction<string | undefined>>) {
+        return (event: NativeSyntheticEvent<TextInputChangeEventData>) => setter(event.nativeEvent.text);
+    }
+
     return (
-        <ScrollView style={{ padding: 16 }}>
-            <Setting.Group label="Name (required)">
-                <Setting.Surface borderColor={getColor(name)}>
-                    <TextInput
-                        onFocus={(e) => console.debug(e.nativeEvent)}
-                        style={style.input}
-                        value={name}
-                        onChange={(e) => setName(e.nativeEvent.text)}
-                        placeholder='Your Name'
-                    />
-                </Setting.Surface>
-            </Setting.Group>
-            <Setting.Group label="Email (required)">
-                <Setting.Surface borderColor={getColor(email)}>
-                    <TextInput
-                        style={style.input}
-                        value={email}
-                        onChange={(e) => setEmail(e.nativeEvent.text)}
-                        placeholder='your.email@example.com'
-                        inputMode="email"
-                    />
-                </Setting.Surface>
-            </Setting.Group>
-            <Setting.Group label="Description (required)">
-                <Setting.Surface borderColor={getColor(description)}>
-                    <TextInput
-                        style={[style.input, style.large]}
-                        value={description}
-                        onChange={(e) => setDescription(e.nativeEvent.text)}
-                        placeholder='Found a bug? Have some feedback? Tell us here...'
-                        multiline={true}
-                    />
-                </Setting.Surface>
-            </Setting.Group>
-            <Setting.Button label="Send feedback" onPress={() => setValidated(!validated)} />
-        </ScrollView>
+        <KeyboardAvoidingView style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', }} behavior="padding" enabled keyboardVerticalOffset={100}>
+            <ScrollView style={{ padding: 16 }}>
+                <Setting.Group label="Name (required)">
+                    <Setting.Surface borderColor={getColor(name)}>
+                        <TextInput
+                            style={style.input}
+                            value={name}
+                            onChange={setValue(setName)}
+                            placeholder='Your Name'
+                        />
+                    </Setting.Surface>
+                </Setting.Group>
+                <Setting.Group label="Email (required)">
+                    <Setting.Surface borderColor={getColor(email)}>
+                        <TextInput
+                            style={style.input}
+                            value={email}
+                            onChange={setValue(setEmail)}
+                            placeholder='your.email@example.com'
+                            inputMode="email"
+                        />
+                    </Setting.Surface>
+                </Setting.Group>
+                <Setting.Group label="Description (required)">
+                    <Setting.Surface borderColor={getColor(description)}>
+                        <TextInput
+                            style={[style.input, style.large]}
+                            value={description}
+                            onChange={setValue(setDescription)}
+                            placeholder='Found a bug? Have some feedback? Tell us here...'
+                            multiline={true}
+                        />
+                    </Setting.Surface>
+                </Setting.Group>
+                <Setting.Button label="Send feedback" onPress={() => setValidated(!validated)} />
+                <View style={{padding: 16}} />
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }
